c560d3a1cfd42ae9e002a0de6077602e5bb345ce
Iam-VM <vishnumohanantheking@gmail.com>
2021-05-17 12:52:43 +0530

    modified env vars

--- src/sequelizeConfig/config.js
+++ src/sequelizeConfig/config.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require('sequelize');
 
-const database = process.env.HEROKU_POSTGRES_DATABASE;
-const username = process.env.HEROKU_POSTGRES_USER;
-const password = process.env.HEROKU_POSTGRES_PASSWORD;
-const host = process.env.HEROKU_POSTGRES_HOST;
+const database = process.env.POSTGRES_DATABASE;
+const username = process.env.POSTGRES_USER;
+const password = process.env.POSTGRES_PASSWORD;
+const host = process.env.POSTGRES_HOST;
 
 const sequelize = new Sequelize(database, username, password, {
     host: host,
